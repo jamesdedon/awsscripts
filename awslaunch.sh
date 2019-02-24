b23883556551ce92d6c7b04ab893b7d23033911d
jamesdedon <james.dedon@gmail.com>
2019-02-24 16:20:02 -0600

    Updated the scripts and readme.md

--- awslaunch.sh
+++ awslaunch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-035be7bafff33b6b6  --count 1 --instance-type t2.micro --key-name seis665_AWS_NorthernVirginia  --security-groups launch-wizard-3
+aws ec2 run-instances --image-id ami-035be7bafff33b6b6  --count 1 --instance-type t2.micro --key-name <your keypair name>  --security-groups launch-wizard-3
--- awslogin.sh
+++ awslogin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ip=$(aws ec2 describe-instances --instance-ids ${1} --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
-ssh -i "seis665_AWS_NorthernVirginia.pem" ec2-user@${ip}
+ssh -i "<your key file>" ec2-user@${ip}
